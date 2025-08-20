710703952c1dbacbbd309747d7399b727924bc28
seock1000 <tjrcjs0705@gmail.com>
2025-08-20 19:58:19 +0900

    [FEAT] ID 생성을 위한 snowflake 알고리즘 ID 생성 모듈

---
+++ common/snowflake/src/main/java/seock1000/board/common/snowflake/Snowflake.java
@@ -0,0 +1,54 @@
+package seock1000.board.common.snowflake;
+
+import java.util.random.RandomGenerator;
+
+/**
+ * 분산 시스템에서 고유한 순차적 ID를 생성하기 위한 Snowflake 알고리즘
+ */
+public class Snowflake {
+	private static final int UNUSED_BITS = 1;
+	private static final int EPOCH_BITS = 41;
+	private static final int NODE_ID_BITS = 10;
+	private static final int SEQUENCE_BITS = 12; // 동일한 시간 내 Unique 보장
+
+	private static final long maxNodeId = (1L << NODE_ID_BITS) - 1;
+	private static final long maxSequence = (1L << SEQUENCE_BITS) - 1;
+
+	private final long nodeId = RandomGenerator.getDefault().nextLong(maxNodeId + 1);
+	// UTC = 2024-01-01T00:00:00Z
+	private final long startTimeMillis = 1704067200000L;
+
+	private long lastTimeMillis = startTimeMillis;
+	private long sequence = 0L;
+
+	// 키 생성 block : synchronized 동시성 보장
+	public synchronized long nextId() {
+		long currentTimeMillis = System.currentTimeMillis();
+
+		if (currentTimeMillis < lastTimeMillis) {
+			throw new IllegalStateException("Invalid Time");
+		}
+
+		if (currentTimeMillis == lastTimeMillis) {
+			sequence = (sequence + 1) & maxSequence;
+			if (sequence == 0) {
+				currentTimeMillis = waitNextMillis(currentTimeMillis);
+			}
+		} else {
+			sequence = 0;
+		}
+
+		lastTimeMillis = currentTimeMillis;
+
+		return ((currentTimeMillis - startTimeMillis) << (NODE_ID_BITS + SEQUENCE_BITS))
+			| (nodeId << SEQUENCE_BITS)
+			| sequence;
+	}
+
+	private long waitNextMillis(long currentTimestamp) {
+		while (currentTimestamp <= lastTimeMillis) {
+			currentTimestamp = System.currentTimeMillis();
+		}
+		return currentTimestamp;
+	}
+}
